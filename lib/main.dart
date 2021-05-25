9d89e92e57467918c3a0c67c422b34005c64c20e
LEONARDHOME\aaron <aaron@lumea.org>
2021-05-24 23:41:58 -0700

    224 - Using Forms & working with form input

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ import './screens/product_detail_screen.dart';
 import './providers/products.dart';
 import 'providers/orders.dart';
 import 'screens/user_products_screen.dart';
+import 'screens/edit_product_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -34,6 +35,7 @@ class MyApp extends StatelessWidget {
           CartScreen.routeName: (ctx) => CartScreen(),
           OrdersScreen.routeName: (ctx) => OrdersScreen(),
           UserProductsScreen.routeName: (ctx) => UserProductsScreen(),
+          EditProductScreen.routeName: (ctx) => EditProductScreen(),
         },
         debugShowCheckedModeBanner: false,
       ),
