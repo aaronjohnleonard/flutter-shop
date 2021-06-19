5d92fe4c14d36034441799df83fe249c6d76fc05
LEONARDHOME\aaron <aaron@lumea.org>
2021-06-19 15:07:12 -0700

    Added global config for google api key

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_complete_guide/providers/cart.dart';
 import 'package:flutter_complete_guide/screens/cart_screen.dart';
 import 'package:flutter_complete_guide/screens/orders_screen.dart';
+import 'package:global_configuration/global_configuration.dart';
 import 'package:provider/provider.dart';
 
 import './screens/products_overview_screen.dart';
@@ -13,7 +14,11 @@ import 'screens/edit_product_screen.dart';
 import 'screens/auth_screen.dart';
 import './providers/auth.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await GlobalConfiguration().loadFromAsset("app_settings");
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
@@ -30,8 +35,11 @@ class MyApp extends StatelessWidget {
           ),
           ChangeNotifierProvider.value(value: Cart()),
           ChangeNotifierProxyProvider<Auth, Orders>(
-              update: (ctx, auth, previousOrders) => Orders(auth.token,
-                  previousOrders == null ? [] : previousOrders.orders)),
+            update: (ctx, auth, previousOrders) => Orders(
+              auth.token,
+              previousOrders == null ? [] : previousOrders.orders,
+            ),
+          ),
         ],
         child: Consumer<Auth>(
           builder: (ctx, auth, _) => MaterialApp(
