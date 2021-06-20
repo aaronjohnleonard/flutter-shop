a29e61a8d5376b04132af4c5fe6238a9fcd86e26
LEONARDHOME\aaron <aaron@lumea.org>
2021-06-20 01:50:10 -0700

    277 - attaching products to users

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -71,9 +71,11 @@ class Products with ChangeNotifier {
     notifyListeners();
   }
 
-  Future<void> fetchAndSetProducts() async {
+  Future<void> fetchAndSetProducts([bool filterByUser = false]) async {
+    final filterString =
+        filterByUser ? 'orderBy="creatorId"&equalTo="$userId"' : '';
     var url =
-        'https://flutter-update-948cc-default-rtdb.firebaseio.com/products.json?auth=$authToken';
+        'https://flutter-update-948cc-default-rtdb.firebaseio.com/products.json?auth=$authToken&$filterString';
     try {
       final response = await http.get(url);
       final extractedData = json.decode(response.body) as Map<String, dynamic>;
@@ -118,6 +120,7 @@ class Products with ChangeNotifier {
             'description': product.description,
             'imageUrl': product.imageUrl,
             'price': product.price,
+            'creatorId': userId,
           },
         ),
       );
